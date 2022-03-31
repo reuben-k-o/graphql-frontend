eb0e101b47670c8a44b3b8d0648b17d282b3d97d
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-31 16:39:40 +0300

    pagination

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -58,7 +58,7 @@ class Feed extends Component {
     const graphqlQuery = {
       query: `
       {
-      getPosts{
+      getPosts(page: ${page}){
         posts{
             _id
             title
@@ -208,6 +208,7 @@ class Feed extends Component {
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedPosts.pop();
             updatedPosts.unshift(post);
           }
           return {
